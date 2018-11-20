71708ffc9731cd66cd967a1fb1b99f445b95db8c
gopinath <rgopinath894@gmail.com>
2018-11-20 15:38:44 +0530

    building blocks of yolo

--- darknet.py
+++ darknet.py
@@ -28,7 +28,7 @@ def parse_cfg(cfgfile):
             if len(block) != 0:          # If block is not empty, implies it is storing values of previous block.
                 blocks.append(block)     # add it the blocks list
                 block = {}               # re-init the block
-            block["type"] = line[1:-1].rstrip()     
+            block["type"] = line[1:-1].rstrip()
         else:
             key,value = line.split("=") 
             block[key.rstrip()] = value.lstrip()
@@ -44,55 +44,53 @@ def create_modules(blocks):
     output_filters = []
 
     for index, x in enumerate(blocks[1:]):
-    module = nn.Sequential()
+        module = nn.Sequential()
 
-    #check the type of block
-    #create a new module for the block
-    #append to module_list
-    if (x["type"] == "convolutional"):
-        #Get the info about the layer
-        activation = x["activation"]
-        try:
-            batch_normalize = int(x["batch_normalize"])
-            bias = False
-        except:
-            batch_normalize = 0
-            bias = True
+        #check the type of block
+        #create a new module for the block
+        #append to module_list
+        if (x["type"] == "convolutional"):
+            # get info about the layer
+            padding = int(x["pad"])
+            stride = int(x["stride"])
+            filters = int(x["filters"])
+            try:
+               batch_normalize = int(x["batch_normalize"])
+               bias = False
+            except:
+                batch_normalize = 0
+                bias = True
 
-        filters= int(x["filters"])
-        padding = int(x["pad"])
-        kernel_size = int(x["size"])
-        stride = int(x["stride"])
+            kernel_size = int(x["size"])
+            activation = x["activation"]
 
-        if padding:
-            pad = (kernel_size - 1) // 2
-        else:
-            pad = 0
+            if padding:
+                pad = (filters - 1) //2    
+            else:
+                pad = 0
 
-        #Add the convolutional layer
-        conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
-        module.add_module("conv_{0}".format(index), conv)
+            # Add convolutional layer
+            conv = nn.Conv2d(prev_filters,filters,kernel_size,stride,pad,bias=bias)
+            module.add_module("conv_{0}".format(index),conv)
 
-        #Add the Batch Norm Layer
-        if batch_normalize:
+            # Add batchnorm layer
             bn = nn.BatchNorm2d(filters)
-            module.add_module("batch_norm_{0}".format(index), bn)
+            module.add_module("batch_norm_{0}".format(index),bn)
 
-        #Check the activation. 
-        #It is either Linear or a Leaky ReLU for YOLO
-        if activation == "leaky":
-            activn = nn.LeakyReLU(0.1, inplace = True)
-            module.add_module("leaky_{0}".format(index), activn)
+            # Add Activation layer
+            if activation == "leaky":
+                activn = nn.LeakyReLU(negative_slope=0.1, inplace=False)
+                module.add_module("leaky_{0}".format(index),activn)
 
-    #If it's an upsampling layer
-    #We use Bilinear2dUpsampling
-    elif (x["type"] == "upsample"):
-        stride = int(x["stride"])
-        upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
-        module.add_module("upsample_{}".format(index), upsample)
-
+        #If it's an upsampling layer
+        #We use Bilinear2dUpsampling
+        elif(x["type"] == "upsample"):
+            stride = int(x["stride"])
+            upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
+        
         #If it is a route layer
-        elif (x["type"] == "route"):
+        elif(x["type"] == "route"):
             x["layers"] = x["layers"].split(',')
             #Start  of a route
             start = int(x["layers"][0])
@@ -106,6 +104,7 @@ def create_modules(blocks):
                 start = start - index
             if end > 0:
                 end = end - index
+
             route = EmptyLayer()
             module.add_module("route_{0}".format(index), route)
             if end < 0:
@@ -118,8 +117,34 @@ def create_modules(blocks):
             shortcut = EmptyLayer()
             module.add_module("shortcut_{}".format(index), shortcut)
 
-    route = EmptyLayer()
+        #Yolo is the detection layer
+        elif x["type"] == "yolo":
+            print(x)
+            mask = x["mask"].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x["anchors"].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    return (net_info, module_list)
 
 class EmptyLayer(nn.Module):
     def __init__(self):
         super(EmptyLayer, self).__init__()
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+blocks = parse_cfg("cfg/yolov3.cfg")
+print(create_modules(blocks))
